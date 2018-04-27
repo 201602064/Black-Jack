ddef0fe6d79049d8df868cf973939db1fec0ed3c
pandapandari <gn04144@cnu.ac.kr>
2018-04-27 20:54:24 +0900

    Blackjack_수정

--- src/main/java/com/cnu/blackjack/Application.java
+++ src/main/java/com/cnu/blackjack/Application.java
@@ -2,24 +2,27 @@ package com.cnu.blackjack;
 
 public class Application {
     public static void main(String args[]){
-        //게임시작할때 게임 이름? 같은거 프린트
+        System.out.println("◇◆ BLACKJACk 게임을 시작합니다 ◆◇");
         Deck deck = new Deck(1);
         Hand hand = new Hand(deck);
         Player player = new Player(10,hand);
         Dealer dealer = new Dealer();
         player.getHand().drawCard();
-        //이거 카드 이쁘게 출력해야해
-        System.out.println(player.getHand().getCardList().get(0).getRank());
+        System.out.print("User Card : " + player.getHand().getCardList().get(0).getRank() + " ");
         System.out.println(player.getHand().getCardList().get(0).getSuit());
         int score = dealer.getScore();
         if(score > 21){
-            //딜러이김
+            System.out.println("☆★ 21점을 넘겨 Dealer가 이겼습니다! ★☆");//딜러이김
         }
         else{
-            System.out.println("현재까지의 점수는 "+score+"입니다.");
+            System.out.println("▷ 현재까지의 점수는 "+score+"입니다.");
             System.out.println(score+player.getHand().getCardList().get(0).getRank());
-            //이때가 21을 안넘을 경우 유저 이김 if
-            //이때가 21을 넘으면 딜러가 이김 else
+            if(score <= 21){
+                System.out.println("☆★ User가 이겼습니다! ★☆★");//이때가 21을 안넘을 경우 유저 이김
+            }
+            else{
+                System.out.println("☆★ Dealer가 이겼습니다! ★☆★");//이때가 21을 넘으면 딜러가 이김
+            }
         }
     }
 }
